b76e9b994cf3a8ac20bb8433369557d9bb4a03a8
bauercedric91@gmail.com <bauercedric91@1b3.cluster>
2025-12-15 10:57:48 +0100

    Just preparation for ex00; Makefile & getting into the subject

---
+++ ex00/inc/BitcoinExchange.hpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+// #include 
+
+// takes csv files as database
+// csv files geht von 2009 bis 2021; jeder Eintrag ist + 3 Tage; 1610 einträge
+// input will be second database "storing the different prices/dates to evaluate."
+
+//rules to respect
