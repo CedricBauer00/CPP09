344951f0ad8806c2b836078eddd165d38393145e
Cedric Bauer <cbauer@3-C-5.42heilbronn.de>
2025-12-18 12:42:22 +0100

    research of the algoithm workflow

--- ex02/src/PmergeMe.cpp
+++ ex02/src/PmergeMe.cpp
@@ -10,6 +10,16 @@ PmergeMe::~PmergeMe()
     std::cout << RED << "PmergeMe destrcutor!" << RESET << std::endl;
 }
 
+int sortList()
+{
+    return 0;
+}
+
+int sortVector()
+{
+    return 0;
+}
+
 int    PmergeMe::merge( const int argc, char **argv ) //konnte nicht const char **argv machen, warum?
 {
     for ( int i = 1; i < argc; ++i )
@@ -25,7 +35,32 @@ int    PmergeMe::merge( const int argc, char **argv ) //konnte nicht const char 
 
     }
 
-    //sorting logic
+    {
+        if ( !sortList() )// std::list container logic
+            return -1;
+    }
+
+    {
+        if ( sortVector() )// std::vector container logic
+            return -1;
+    }
+
     std::cout << GREEN << "Done!" << RESET << std::endl; 
     return 0;
 }
+
+// sort number of sequence into pairs of numbers, then in pairs of pairs, then into pairs of pairs of pairs, ... until there is nothing to still pair
+
+// if number of elements is uneven, let the last one be
+
+// create a sequence out of smallest element (b) of smallest pair (b1) and the rest of a's
+// => the main
+
+// create a sequence out of rest of b's and IF  there is a odd element, this as well, if not then not.
+// => the pend
+
+// binary insert elements form pend into the main; numbers are based on jacobsthal numbers???
+
+// insert them in reverse order if not able to do the jacobsthal numbers anymore
+
+// 
