a23a0ff81b3b84bfeab75b97701bf8a99062666a
Cedric Bauer <cbauer@3-C-5.42heilbronn.de>
2025-12-18 09:41:39 +0100

    started with ex02, getting into subject

---
+++ ex02/inc/PmergeMe.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <list>
+#include <iostream>
+#include <exception>
+
+#define RED  "\033[31m"
+#define ELEC_RED "\033[38;2;255;20;20m"
+#define BLUE    "\033[34m"
+#define GREEN  "\033[32m"
+#define RESET  "\033[0m"
+#define ORANGE  "\033[38;2;255;120;0m"
+
+class PmergeMe
+{
+private:
+    
+public:
+    PmergeMe();
+    PmergeMe( const PmergeMe &copy );
+    PmergeMe& operator=( const PmergeMe &copy );
+    ~PmergeMe();
+};
+
+// handles positive range of integers
+// must use the Ford Johnson algo - Merge insertion??
+// if error occurs during runtime, appropriate error message on std::cerr
+// use 2 different containers 
+// program must be able to use at least 300 different integers
+// implement algo for each container
+// avoid using generic functions
+// output:
+// 1 line: explicit text, unsorted positive int sequence
+// 2 line: explicit text, sorted positive int sequence 
+// 3 line: explicit message indicating the time the algo took - specifying FIRST container
+// 3 line: explicit message indicating the time the algo took - specifying SECOND container
+
+// format of time display is free - difference between container time must be visible though
+// operation time includes all ops - sorting and data management
+// no std::map or std::stack
+// "The management of errors related to duplicates is left to your discretion."
---
+++ ex02/src/main.cpp
@@ -0,0 +1,6 @@
+#include "../inc/PmergeMe.hpp"
+
+PmergeMe::PmergeMe()
+{
+    std::cout << GREEN << "A"
+}
